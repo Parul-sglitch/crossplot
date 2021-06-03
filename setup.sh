d2ea95f30c6822f5634105717a3364b6e6c188d7
Parul Singh <78781760+Parul-sglitch@users.noreply.github.com>
2021-06-03 20:16:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"parulsingh19.rs@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
